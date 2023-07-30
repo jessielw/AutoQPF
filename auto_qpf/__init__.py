5e9de7d9bddb7fe993ebfa6515ec69bee4c2aac2
jlw4049 <jlw_4049@hotmail.com>
2023-07-30 12:39:24 -0400

    - Add ChapterGenerator

--- auto_qpf/__init__.py
+++ auto_qpf/__init__.py
@@ -1,4 +1,5 @@
 from .qpf import QpfGenerator as QpfGenerator
+from .qpf import ChapterGenerator as ChapterGenerator
 from .qpf_exceptions import ChapterIndexError as ChapterIndexError
 from .qpf_exceptions import ImproperChapterError as ImproperChapterError
 from .qpf_exceptions import NoChapterDataError as NoChapterDataError
